1a0d484fdeb249b99f86f1d63bbe2da5224554a2
hbblzjy <hbblzjy@163.com>
2017-07-12 15:31:53 +0800

    Initial Commit

---
+++ NSDataDetector/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSDataDetector
+//
+//  Created by JasonHao on 2017/7/12.
+//  Copyright © 2017年 JasonHao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NSDataDetector/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NSDataDetector
+//
+//  Created by JasonHao on 2017/7/12.
+//  Copyright © 2017年 JasonHao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
